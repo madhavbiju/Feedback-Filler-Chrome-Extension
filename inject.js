c85ee405ace7ef419f6a1691dbc1ef9d12cd630b
Madhav Biju <65170463+madhavbiju@users.noreply.github.com>
2022-08-13 15:50:12 +0530

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,19 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+document.querySelector('input[value="41"]').checked=true;
+document.querySelector('input[value="37"]').checked=true;
+document.querySelector('input[value="33"]').checked=true;
+document.querySelector('input[value="29"]').checked=true;
+document.querySelector('input[value="27"]').checked=true;
+document.querySelector('input[value="23"]').checked=true;
+document.querySelector('input[value="19"]').checked=true;
+document.querySelector('input[value="16"]').checked=true;
+document.querySelector('input[value="12"]').checked=true;
+document.querySelector('input[value="9"]').checked=true;
+document.querySelector('input[value="6"]').checked=true;
+document.querySelector('input[value="Submit"]').disabled = false;
+
+
+})();
